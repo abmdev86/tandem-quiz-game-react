6f75a822ee47f60559a28399dfb0bd8a28b7591b
Auroiah Morgan <amwebdev86@outlook.com>
2020-11-01 19:49:41 -0500

    Fixed scores and end of round

--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -6,12 +6,28 @@ import React from 'react'
    */
 
 const ScoreCard = (props) => {
-  return (
-        <div >
-            <h3>You scored {props.score} out of {props.questions.length}</h3>
-           <button onClick={(e)=>props.startRound(e)}>Next Round</button>
+   console.log(props.isRound1)
+   if (props.isRound1)
+   {
+        return (
+          <div>
+            <h3>
+              You scored {props.score} out of {props.questions.length - 1}
+            </h3>
+            <button onClick={(e) => props.startRound(e)}>Next Round</button>
+          </div>
+        );
+   } else {
+      return (
+        <div>
+          <h3>
+            You scored {props.score - 10} out of {props.questions.length}
+          </h3>
+          <button onClick={(e) => props.startRound(e)}>Next Round</button>
         </div>
-  )
+      );
+      }
+
 }
 
 export default ScoreCard
