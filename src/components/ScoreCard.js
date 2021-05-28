9162f80a8e69d83dd937bb2c56bc23c390fbfcdf
Auroiah Morgan <amwebdev@outlook.com>
2021-05-27 23:10:21 -0400

    Update ScoreCard.js

--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -14,26 +14,7 @@ export default function ScoreCard(props) {
       </div>
     </div>
   );
-  //  if (props.isRound1)
-  //  {
-  //       return (
-  //         <div>
-  //           <h3>
-  //             You scored {props.score} out of {props.questions.length - 1}
-  //           </h3>
-  //           <button onClick={(e) => props.startRound(e)}>Next Round</button>
-  //         </div>
-  //       );
-  //  } else {
-  //     return (
-  //       <div>
-  //         <h3>
-  //           You scored {props.score > 11 ?  props.score - 10: props.score} out of {props.questions.length}
-  //         </h3>
-  //         <button onClick={() => props.gameOver()}>End</button>
-  //       </div>
-  //     );
-  //     }
+
 
 }
 
