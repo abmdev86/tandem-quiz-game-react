6031b6f268c46e558c13292b2cda41c90add47a7
Auroiah Morgan <amwebdev@outlook.com>
2021-05-29 06:00:32 -0400

    Updates Questions

--- src/App.js
+++ src/App.js
@@ -2,69 +2,10 @@ import React, { useState } from "react";
 import QuestionCard from "./components/QuestionCard";
 import ErrorBoundary from "./components/ErrorBoundary";
 import ScoreCard from "./components/ScoreCard";
+import { questions } from './data';
 //import SummaryCard from "./components/SummaryCard";
 
 export default function App() {
-  /**
-   * An array of question objects which contain a questionText that holds the questions to display
-   * and an answers array with answer objects
-   * these answer objects contain answerText that holds the answer to be displayed and a bool isCorrect.
-   *
-   */
-  const questions = [
-    {
-      questionText: "What is Binary?",
-      answers: [
-        {
-          answerText: "a Base2 system",
-          isCorrect: true,
-        },
-        {
-          answerText: "A base-10 system",
-          isCorrect: false,
-        },
-      ],
-    },
-    {
-      questionText: "What is the Decimal system?",
-      answers: [
-        {
-          answerText: "a Base2 system",
-          isCorrect: false,
-        },
-        {
-          answerText: "A base-10 system",
-          isCorrect: true,
-        },
-      ],
-    },
-    {
-      questionText: "What is Hexadecimal?",
-      answers: [
-        {
-          answerText: "a base-16 system",
-          isCorrect: true,
-        },
-        {
-          answerText: "A base-10 system",
-          isCorrect: false,
-        },
-      ],
-    },
-    {
-      questionText: "What is 1010 in Decimal?",
-      answers: [
-        {
-          answerText: "10",
-          isCorrect: true,
-        },
-        {
-          answerText: "1100",
-          isCorrect: false,
-        },
-      ],
-    }
-  ];
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
---
+++ src/data/index.js
@@ -0,0 +1,98 @@
+export const questions = [
+  {
+    "questionText": "What is Binary?",
+    "answers": [
+      {
+        "answerText": "A number expressed in the base-2 numeral system",
+        "isCorrect": true
+      }, {
+        "answerText": "A number expressed in the base-10 numeral system",
+        "isCorrect": false
+      }, {
+        "answerText": "A number expressed in the base-16 numeral system",
+        "isCorrect": false
+      }, {
+        "answerText": "It is a number doubled",
+        "isCorrect": false
+      }
+    ]
+  },
+  {
+    questionText: "What is Decimal?",
+    "answers": [
+      {
+        "answerText": "A number expressed in the base-2 numeral system",
+        "isCorrect": false
+      }, {
+        "answerText": "A number expressed in the base-10 numeral system",
+        "isCorrect": true
+      }, {
+        "answerText": "A number expressed in the base-16 numeral system",
+        "isCorrect": false
+      }, {
+        "answerText": "It is a number doubled",
+        "isCorrect": false
+      }
+    ]
+  },
+  {
+    questionText: "What is Hexadecimal?",
+    "answers": [
+      {
+        "answerText": "A number expressed in the base-2 numeral system",
+        "isCorrect": false
+      }, {
+        "answerText": "A number expressed in the base-10 numeral system",
+        "isCorrect": false
+      }, {
+        "answerText": "A number expressed in the base-16 numeral system",
+        "isCorrect": true
+      }, {
+        "answerText": "It is a number with 16-sides",
+        "isCorrect": false
+      }
+    ],
+  },
+  {
+    questionText: "The Binary: 1010, Translates to what Decimal number?",
+    answers: [
+      {
+        answerText: "10",
+        isCorrect: true,
+      },
+      {
+        answerText: "1100",
+        isCorrect: false,
+      },
+      {
+        answerText: "1",
+        isCorrect: false,
+      },
+      {
+        answerText: "18",
+        isCorrect: false,
+      }
+    ],
+  },
+  {
+    questionText: "What are Logic Gates?",
+    answers: [
+      {
+        answerText: "An electronic component that can be used to conduct electricity based on a rule.",
+        isCorrect: true,
+      },
+      {
+        answerText: "A popular music making program",
+        isCorrect: false,
+      },
+      {
+        answerText: "Gates that argue logic",
+        isCorrect: false,
+      },
+      {
+        answerText: "Electrical currents with tight security",
+        isCorrect: false,
+      }
+    ],
+  }
+];
