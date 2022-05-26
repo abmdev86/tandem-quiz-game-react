c6d60a60e6f65cf67e849a2a7e0f4de97b13de3e
Auroiah Morgan <amwebdev@outlook.com>
2022-05-25 19:20:25 -0500

    Update QuestionCard.js

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,12 +1,10 @@
 /* eslint-disable react/prop-types */
-//import React, { Fragment } from "react";
-import React from "react";
-import ID from '../Utils/helpers';
-import { sha256 } from 'js-sha256';
 
+import React from "react";
+import ID from "../Utils/helpers";
+import { sha256 } from "js-sha256";
 
 export default function QuestionCard(props) {
-
   return (
     <div className="question-card">
       {props.showCard ? (
@@ -16,22 +14,24 @@ export default function QuestionCard(props) {
       ) : (
         <div className="question-display">
           <div className="question-counter">
-
-            <span>Current Question:{props.currentQuestion + 1}</span>/{props.questions.length}
+            <span>Current Question:{props.currentQuestion + 1}</span>/
+            {props.questions.length}
           </div>
           <div className="question-section">
             {props.questions[props.currentQuestion].questionText}
           </div>
           <div className="answer-section">
             {props.questions[props.currentQuestion].answers.map((answer) => (
-              <button onClick={() => props.handleAnswerOptionClick(answer.isCorrect)} key={sha256(ID())}>{answer.answerText}</button>
+              <button
+                onClick={() => props.handleAnswerOptionClick(answer.isCorrect)}
+                key={sha256(ID())}
+              >
+                {answer.answerText}
+              </button>
             ))}
           </div>
         </div>
-
       )}
     </div>
   );
-
 }
-
