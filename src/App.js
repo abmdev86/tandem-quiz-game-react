aaa24b1a6b53052cfb6e0cb77f35bd690a3a2c80
Auroiah Morgan <amwebdev@outlook.com>
2022-05-25 19:20:16 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { useState } from "react";
 import QuestionCard from "./components/QuestionCard";
 import ErrorBoundary from "./components/ErrorBoundary";
 import ScoreCard from "./components/ScoreCard";
-import { questions } from './data';
+import { questions } from "./data";
 //import SummaryCard from "./components/SummaryCard";
 
 export default function App() {
-
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
@@ -17,7 +16,6 @@ export default function App() {
     }
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < questions.length) {
-
       setCurrentQuestion(nextQuestion);
     } else {
       setShowScore(true);
@@ -26,14 +24,17 @@ export default function App() {
 
   return (
     <ErrorBoundary>
-
-      { !showScore ? (<QuestionCard score={score} questions={questions} showScore={showScore} currentQuestion={currentQuestion}
-        handleAnswerOptionClick={handleAnswerOptionClick} />) : (
+      {!showScore ? (
+        <QuestionCard
+          score={score}
+          questions={questions}
+          showScore={showScore}
+          currentQuestion={currentQuestion}
+          handleAnswerOptionClick={handleAnswerOptionClick}
+        />
+      ) : (
         <ScoreCard score={score} questions={questions} />
       )}
     </ErrorBoundary>
   );
-
-
 }
-
